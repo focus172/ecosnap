7f3bd06a6a7e94010ede1c15efae2e07b4879642
Focus172 <evan.stokdyk@gmail.com>
2024-02-17 12:25:32 -0800

    Cache Data

--- main.py
+++ main.py
@@ -1,45 +1,31 @@
 from flask import Flask, request, jsonify
 import json
 
-
+# Init
 app = Flask(__name__)
-
-# loading the json file
-with open ("data.json", 'r') as file:
+with open("data.json", "r") as file:
     data = json.load(file)
+
 
 @app.route("/search", methods=["POST"])
 def search():
-    data = request.json
-    if data is None:
-        return jsonify({"result": "Error missing request."})
-    name = data["name"]
+    req = request.json
+    if req is None:
+        return jsonify({"error": "Error missing request."})
+
+    name = req["name"]
     if name is None:
-        return jsonify({"result": "Error bad formated request."})
+        return jsonify({"error": "Error bad formated request."})
 
-    file = open("data.json", "r")
-    parsed = json.load(file)
-    score = parsed[name]
-
+    score = data[name]
     if score is None:
-        return jsonify({"result": "Could not find thing thing."})
-    # print(score)
-    #
-    file.close()
+        return jsonify({"error": "Could not find thing thing."})
+
     return jsonify({"result": score})
 
 
-@app.route("/getScores/<name>")
-def getScores(name):
-
-    if (name in data):
-        return str(data[name])
-        
-    return f"Company '{name}' Not Found"
-
-def main():
-    search()
-
-
-if __name__ == "__main__":
-    main()
+# def main():
+#     search()
+#
+# if __name__ == "__main__":
+#     main()
